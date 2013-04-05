983a30ebb5ed5a15b7f9563b1eee84976cd3f408
Nicolas Goles <goles.nicolas@gmail.com>
2013-04-05 08:59:41 -0300

    Increased version to 1.1, this fixes a slight detail on the status display for tmux.

--- battery.rb
+++ battery.rb
@@ -4,7 +4,7 @@ class Battery < Formula
   homepage 'https://github.com/Goles/Battery'
   url 'https://raw.github.com/Goles/Battery/master/battery'
   sha1 '567446640f9435ce22c3efe5927cb5753080a797'
-  version '1.0'
+  version '1.1'
 
   depends_on 'spark'
 
