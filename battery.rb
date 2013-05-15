e22f2cdf6af087877e86a73e0326ec82785e8e62
Ivan Valdes (@ivanvc) <iv@nvald.es>
2013-05-15 16:32:26 -0500

    Update md5

--- battery.rb
+++ battery.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Battery < Formula
   homepage 'https://github.com/ivanvc/Battery'
   url 'https://raw.github.com/ivanvc/Battery/master/battery'
-  sha1 '47f6dad276c2455280413254deb6bbc47d00d90a'
+  md5 '373297c0f61df437251b9d679e072d8b'
   version '1.1.1'
 
   depends_on 'spark'
