079c3acd974d98774a283f772f5ff1c233ebc7aa
Ivan Valdes (@ivanvc) <iv@nvald.es>
2013-05-15 16:34:03 -0500

    Update sha1, remove md5

--- battery.rb
+++ battery.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Battery < Formula
   homepage 'https://github.com/ivanvc/Battery'
   url 'https://raw.github.com/ivanvc/Battery/master/battery'
-  md5 '373297c0f61df437251b9d679e072d8b'
+  sha1 'b9818057de2ab6693dd112f307550ff5d826113e'
   version '1.1.1'
 
   depends_on 'spark'
